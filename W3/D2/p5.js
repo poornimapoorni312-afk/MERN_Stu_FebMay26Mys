b8285101db136b5bafd15d353e53ec02a36668d6
unknown <poornimapoorni312@gmail.com>
2026-03-03 22:38:06 +0530

    Function declaration

--- W3/D2/p5.js
+++ W3/D2/p5.js
@@ -1,10 +1,10 @@
-// Function declaration
+// Function declaration...
  //funtion greet(name);
  greet ("poornima");
 //Function Declaration/defination
 function greet(name) {
     console.log("Hello,"+name+" !"); 
-    
+
 }
 //Function call
 greet("poornima");
