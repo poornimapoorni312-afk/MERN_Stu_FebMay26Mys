856c0a04f15a7348a76e77317d386f6a5dce12a6
unknown <poornimapoorni312@gmail.com>
2026-03-03 22:44:15 +0530

    immediately Invoked Function Expression(IIFE)

---
+++ W3/D2/p9.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expression(IIFE)
+
+
+    (function() {
+        console.log("Basic IIFE executed Immediately");
+    })();
+
+    (function(appName,version) {
+        console.log("App:",appName,"Version:", version);
+    })("NodeJs","V22.22.0");
+
+    //With return value
+    const result = (function() {
+        const a = 10, b= 20;
+        return a+b;
+    })();
+    console.log("Sum is:",result);
+
+    
+    
