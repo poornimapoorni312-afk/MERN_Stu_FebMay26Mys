e263a33c58ab548658b28736f060f39cfa9c9756
unknown <poornimapoorni312@gmail.com>
2026-03-03 15:46:57 +0530

    Conditional statement

---
+++ W3/D2/p4.js
@@ -0,0 +1,31 @@
+//Conditional statements
+//let age = 20;
+ //if(age <13){
+   //console.log("Child");
+ //}
+//else if(age<18) {
+  // console.log("Teenage");}
+//else
+//{
+  //  console.log("Adult");
+//}
+//Swith statements
+//console.log("Conditional statement");
+//let day = "Tuesday";
+//switch (day) {
+  //  case "Monday":
+    //    console.log("start of the week");
+      //  break;
+        //case "Wednesday":
+        //
+        // console.log("mid of the week");
+      //  case "Friday":
+        //    console.log("End of work week");
+          //  default:
+            //    console.log("Some day in the week");
+              //  break;
+//}
+// Type conversion
+let n = Number("123");
+console.log("Type of n:",typeof n, "isNaN",isNaN(n));
+
