6d4597aa7ffb3631887200f7a7faabab554558e6
unknown <poornimapoorni312@gmail.com>
2026-03-09 09:39:25 +0530

    This , js

---
+++ W3/D5/p11.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("compareBtn");
+
+btn.addEventListener("click",function(e) {
+    console.log("Regular function");
+    console.log("this ===btn",this === btn);
+    console.log("current target === btn",e.currentTarget===btn);
+     console.log("e target === btn",e.target === btn);
+ 
