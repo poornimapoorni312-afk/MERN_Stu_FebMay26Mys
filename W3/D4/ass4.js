f10f972de4d13860de3770b7c9de79d17f5b7a77
unknown <poornimapoorni312@gmail.com>
2026-03-15 20:35:27 +0530

    loggin check

---
+++ W3/D4/ass4.js
@@ -0,0 +1,33 @@
+const getRoute = function(role, isLoggedIn) {
+
+    // login check
+    if(!isLoggedIn){
+        return "/login";
+    }
+
+    // role routing
+    switch(role){
+
+        case "admin":
+            return "/admin";
+
+        case "student":
+            return "/student";
+
+        case "college":
+            return "/college";
+
+        case "proctor":
+            return "/proctor";
+
+        default:
+            return "/denied";
+    }
+};
+
+
+// Example
+console.log(getRoute("admin", true));     // /admin
+console.log(getRoute("student", true));   // /student
+console.log(getRoute("guest", true));     // /denied
+console.log(getRoute("admin", false));    // /login
