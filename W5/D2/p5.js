966086488a0ffdb1b8f02962fcc2f27984fa1855
poornima <poornimapoorni312@gmail.com>
2026-03-17 13:32:10 +0530

    Callback error

---
+++ W5/D2/p5.js
@@ -0,0 +1,15 @@
+function divideNumbers(a,b,callback) {
+    if(b==0) {
+        callback("Cannot divide by zero.", null); return;
+    }
+    const result = a/b;
+    callback(null, result);
+}
+
+divideNumbers(10,2,function(error,result) {
+    if(error) {
+        console.log("Error:", error);
+        return;
+    }
+    console.log("Result", result);
+});
