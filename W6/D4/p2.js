daf4b92860c8074f0ad79584535cdb95ef7e1f54
poornima <poornimapoorni312@gmail.com>
2026-03-26 10:24:00 +0530

    Creating a simple HTTp server

---
+++ W6/D4/p2.js
@@ -0,0 +1,22 @@
+//Creating a simple HTTP server
+
+const http = require("http");
+
+//createServer(): creates a HTTP server instance
+// Accepts a callback with two important objects:
+// 1.  req : incoming request details
+//2. res: outgoing response control 
+
+const server =  http.createServer(function(req,res){
+    //WriteHead() Sets the response status code and headers
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    // end() sends the response body and closes the response
+    res.end("Hello from NodeJS HTTP sever.");
+});
+
+//listen() binds the server to a port and
+//  starts accepting requests
+server.listen(3000,function() {
+    console.log("Server is running at http://localhost:3000");
+});
+
