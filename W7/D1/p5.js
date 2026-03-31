af375af23e5cbce9358a6b4ea28527e684af7c62
poornima <poornimapoorni312@gmail.com>
2026-03-31 14:01:08 +0530

    Middleware  usage in Express

---
+++ W7/D1/p5.js
@@ -0,0 +1,28 @@
+// Middleware usage in Express
+//Middleware runs during the request-response cycle
+//Middle can inspect or change the request before
+//a route responds 
+//next() passes control to the next step
+
+const express = require("express");
+
+const app = express();
+
+//Global middleware runs for every request 
+app.use(function(req,res,next){
+    console.log("Global middleware.",req.method,req.url);
+ 
+   
+    next();
+
+});
+app.get("/admin",function(req,res,next){
+    console.timeLog("Route specific middleWare for / admin");
+    next();
+},function(req,res) {
+    res.send("Admin route reached.");
+});
+
+app.listen(4000, function () {
+    console.log("Express server running at http://localhost:4000");
+});
