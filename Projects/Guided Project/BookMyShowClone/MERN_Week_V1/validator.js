2ca091bdf5b7e09a7320d8459b65ea9f2d64f76c
poornima <poornimapoorni312@gmail.com>
2026-04-06 22:56:00 +0530

    Callback based validation functions

--- Projects/Guided Project/BookMyShowClone/MERN_Week_V1/validator.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week_V1/validator.js
@@ -1,32 +1,36 @@
-//Callback based validation functions
+// Callback based validation functions
 
-function validateMoviesSelection(movies,movieId,callback){
+function validateMovieSelection(movies,movieId,callback) {
+    const selectedMovie = movies.find((movie)=>movie.id === movieId);
 
-    const selectedMovie = movies.find((movie)=>movie.id ===movieId);
-    if(!selectedMovie) {
-        return callback("Invalid movie selection. choose a valid movie ID.",null);
+    if (!selectedMovie) {
+        return callback("Invalid movie selection.Choose a valid movie ID.",null);
     }
     callback(null,selectedMovie);
+
 }
 
-function validateMoviesSelection(movies,selectedTime,callback){
+function validateTimeSelection(movie, selectedTime, callback) {
+  const selectedShowtime = movie.showtimes.find(
+    (show) => show.time.toLowerCase() === selectedTime.toLowerCase()
+  );
 
-    const selectedShowtime = movie.showtimes.find((show)=>show.time.toLowerCase()===selectedTime.toLowerCase);
-    if(!selectedShowtime) {
-        return callback("Invalid time slot  selection. choose a valid time slot ID.",null);
-    }
-    callback(null,selectedShowtime);
+  if (!selectedShowtime) {
+    return callback("Invalid time slot selection. Please choose a valid show time.", null);
+  }
+
+  callback(null, selectedShowtime);
 }
 
-function validateSeatCount(seatCount,callback){
 
-    const selectedShowtime = movie.showtimes.find((show)=>show.time.toLowerCase()===selectedTime.toLowerCase);
-    if(!isNaN(seatCount) || seatCount <= 0){
-        return callback("Invalid seat count . Enter a valid seat count ID.",null);
+function validateSeatCount(seatCount,callback) {
+    if (isNaN(seatCount) || seatCount <= 0) {
+        return callback("Invalid seat count.Enter a valid seat count.",null);
     }
     callback(null,seatCount);
 }
-module.exports= {
+
+module.exports = {
     validateMovieSelection,
     validateTimeSelection,
     validateSeatCount
