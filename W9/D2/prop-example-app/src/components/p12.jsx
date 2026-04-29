49b9af5b231e2f564c4d2d60b06f158596d25c23
poornima <poornimapoorni312@gmail.com>
2026-04-29 15:14:09 +0530

    Higher order components(HOC)

---
+++ W9/D2/prop-example-app/src/components/p12.jsx
@@ -0,0 +1,24 @@
+// Higher order components(HOC)
+const Greeting = ({name}) => {
+    return <h1>Hello,{name}</h1>
+}
+const WithBorder = (OriginalComponent) =>{
+    return function EnhancedComponent(props){
+        return(
+            <div className="setBorder">
+                <OriginalComponent{...props} />
+
+            </div>
+        );
+        
+    }
+}
+const GreetingWithBorder = WithBorder(Greeting);
+
+export function HOCExample(){
+    return(
+        <>
+        <GreetingWithBorder name="Poorni"/>
+        </>
+    )
+}
