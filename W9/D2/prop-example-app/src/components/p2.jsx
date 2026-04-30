b29215350afe6b920688c567fcca135d0a9e1264
poornima <poornimapoorni312@gmail.com>
2026-04-30 09:13:05 +0530

    comments added

--- W9/D2/prop-example-app/src/components/p2.jsx
+++ W9/D2/prop-example-app/src/components/p2.jsx
@@ -1,3 +1,22 @@
-// Props destrucuring
-
+// Props destructuring
+//a syntax of Es6 approach that allows us to unpack properties from
+//prop objects directly into its value
 import { React } from "react";
+//Child component
+function UserProfile({username,skill}){
+    return(
+        <div>
+            <p>User: {username}</p>
+            <p>Skill: {skill}</p>
+        </div>
+    )
+}
+// Parent component
+export function PropDestructuring(){
+    return(
+        <>
+        <h2>Prop Destructuring</h2>
+        <UserProfile username="Poornima" skill="React" />
+        </>
+    );
+}
