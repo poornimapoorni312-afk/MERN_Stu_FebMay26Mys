96f39bf160c979dcefa90c12d75aa09c61fa402e
poornima <poornimapoorni312@gmail.com>
2026-05-07 16:00:35 +0530

    Accessibility for custom Clickable elements

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
 
-import { EventBasics } from "./components/p1";
+//import { EventBasics } from "./components/p1";
+//import { EventObject } from "./components/p2";
+//import { PassingArguments } from "./components/p3";
+//import {UseCallbackEvents} from "./components/p5";
+//import {EventDelegation}from"./components/p6";
+//import { FormsPattern}from"./components/p7";
+//import{KeyboardEvent}from"./components/p8";
+// import {EventObj} from "./components/p9";
+import{Accessibility}from"./components/p10";
+
 function App() {
   return (
     <>
-      <EventBasics />
+      {/* <EventBasics /> */}
+      {/* <EventObject /> */}
+      {/* <PassingArguments /> */}
+      {/* <UseCallbackEvents/> */}
+      {/* <EventDelegation/> */}
+      {/* < FormsPattern/> */}
+      {/* <KeyboardEvent/> */}
+      {/* <EventObj/> */}
+      <Accessibility/>
     </>
   )
 }
-export default App
+
+export default App;
