9eb8625e4ef8d49e5d8bb364de495c971e0b63a7
poornima <poornimapoorni312@gmail.com>
2026-05-12 11:41:48 +0530

    changes added

--- W10/D1/state-hook-app/src/App.jsx
+++ W10/D1/state-hook-app/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
-import {UseStateBasics} from "./components/p1"
-
+//import {UseStateBasics} from "./components/p1"
+//import{UpdatingObjectsArraysState} from "./components/p2"
+//import{EventHandlingState} from"./components/p3"
+//import{DerivedStateCondRender} from"./components/p4"
+import{UseEffectBasics} from"./components/p5"
 function App() {
   return(
     <>
-       <UseStateBasics />
+      {/* <UseStateBasics /> */}
+       {/* <UpdatingObjectsArraysState/> */}
+       {/* <EventHandlingState/> */}
+       {/* <DerivedStateCondRender/> */}
+       <UseEffectBasics/>
     </>
   )
 }
