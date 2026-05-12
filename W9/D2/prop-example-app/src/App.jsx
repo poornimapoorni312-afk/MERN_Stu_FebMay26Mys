0cb924ef84093a1e3b2f152448f2e68a8c030a54
poornima <poornimapoorni312@gmail.com>
2026-05-12 11:40:33 +0530

    W9 prop Example app created

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import { PropBasics } from './Components/p1'
 import './App.css'
-
+// import { PropBasics } from "./components/p1";
+// import { PropDestructuring } from "./components/p2";
+// import { PropsChildren } from "./components/p3";
+// import { PropDrillingDemo } from "./components/p4";
+// import{SharedStateParent} from"./components/p5";
+//import {CompositionDemo} from"./components/p6";
+//import {ObjectAndArrayProps} from"./components/p7";
+//import{ PropsImmutabality} from"./components/p8";
+// import{ PropsState} from"./components/p9";
+// import{ FunctionProps} from"./components/p10";
+// import{ PropTypesDemo} from"./components/p11";
+import{HOCExample} from"./components/p12";
 function App() {
-
   return (
     <>
-      <PropBasics/>
+      {/* <PropBasics /> */}
+      {/* <PropDestructuring /> */}
+      {/* <PropsChildren /> */}
+      {/*<PropDrillingDemo/>*/}
+      {/*<SharedStateParent/>*/}
+      {/*<CompositionDemo/>*/}
+      {/*<ObjectAndArrayProps/>*/}
+      {/*<PropsImmutabality/>*/}
+      {/* < PropsState/> */}
+      {/* <FunctionProps /> */}
+      {/* < PropTypesDemo/> */}
+      <HOCExample/>
     </>
   )
 }
-
-
 export default App
