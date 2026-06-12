9980561bee86b461df21d0325b3d264533778886
poornima <poornimapoorni312@gmail.com>
2026-06-12 12:04:38 +0530

    Usage of Props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,10 +1,37 @@
 import './App.css'
-import HomePage from "./pages/HomePage";
-function App() {
-  return (
-     <HomePage/>
+import MovieCard from './components/MovieCard';
 
-  );
+function App() {
+  
+
+  return (
+    <>
+      <MovieCard 
+      title="Inception"
+      genre="Sci-Fi"
+      rating={4.8}
+      duration="2h 28m"
+      poster="https://picsum.photos/200/300?1" 
+      />,
+      <MovieCard 
+      title="Avatar"
+      genre="Sci-Fi"
+      rating={4.8}
+      duration="2h 28m"
+      poster="https://picsum.photos/200/300?1" 
+      />,
+      <MovieCard 
+      title="FightClub"
+      genre="Sci-Fi"
+      rating={4.8}
+      duration="2h 28m"
+      poster="https://picsum.photos/200/300?1" 
+      />
+    </>
+
+  )
 }
 
 export default App
+
+
